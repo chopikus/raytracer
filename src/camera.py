5f3e8dddf1a895571bcf47ff2e2c02d5245dd64d
Ihor Chovpan <i.chovpan@gmail.com>
2024-08-05 20:10:48 -0400

    Finish a prototype

--- src/camera.py
+++ src/camera.py
@@ -3,6 +3,7 @@ from geom import *
 from sphere import *
 import numpy as np
 from typing import List
+from numba import jit
 
 class Camera:
     image_width: int
@@ -13,8 +14,10 @@ class Camera:
     dv: Vec3
     pixel_samples: int
     depth: int
+    samples_one_time: int
 
-    def __init__(self, aspect_ratio: float, image_width: int, pixel_samples: int) -> None:
+    def __init__(self, aspect_ratio: float, image_width: int, pixel_samples: int, samples_per_one_time: int) -> None:
+        self.samples_one_time = samples_per_one_time
         self.depth = 10
         self.pixel_samples = pixel_samples
         self.image_width = image_width
@@ -55,8 +58,8 @@ class Camera:
 
         return np.where(hits == np.inf, bg, sphere)
 
-    def render(self, world: List[Sphere]) -> None:
-        ray_count = self.image_height * self.image_width * self.pixel_samples
+    def render_pixels(self, world: List[Sphere], pixel_samples: int) -> FloatArray:
+        ray_count = self.image_height * self.image_width * pixel_samples
         centers = PointArray.repeat(self.center, ray_count)
         x_offsets = np.random.uniform(-0.5, 0.5, ray_count)
         y_offsets = np.random.uniform(-0.5, 0.5, ray_count)
@@ -72,8 +75,8 @@ class Camera:
           2. ys = [0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2]
         """
         xfirst = np.repeat(np.arange(self.image_width), self.image_height)
-        xs: FloatArray = np.tile(xfirst, self.pixel_samples)
-        ys: FloatArray = np.tile(np.arange(self.image_height), self.image_width * self.pixel_samples)
+        xs: FloatArray = np.tile(xfirst, pixel_samples)
+        ys: FloatArray = np.tile(np.arange(self.image_height), self.image_width * pixel_samples)
         zs: FloatArray = np.zeros(ray_count)
 
         vecs: Vec3Array = Vec3Array(xs + x_offsets, ys + y_offsets, zs)
@@ -85,16 +88,25 @@ class Camera:
 
         rays = RayArray(centers, ray_directions)
         colors = self.render_rays(rays, world)
-        print(colors)
 
-        """
+        print(colors.size, pixel_samples)
+        colors_splitted = np.split(colors, pixel_samples, axis=1)
+        return np.add.reduce(colors_splitted) / pixel_samples
+        
+    def render(self, world: List[Sphere]) -> None:
+        colors = np.zeros((3, self.image_width * self.image_height))
+        times = self.pixel_samples // self.samples_one_time
+        for _ in range(times):
+            colors += self.render_pixels(world, self.samples_one_time)
+        colors /= times
+
         img = Image(self.image_width, self.image_height)
 
+        i: int = 0
         for x in range(self.image_width):
             for y in range(self.image_height):
-                c = self.render_pixel(world, x, y)
+                c = Color(colors[0][i], colors[1][i], colors[2][i])
                 img.set_pixel(x, y, c)
+                i += 1
 
         img.save("output.png")
-        """
-        
